593f6b17681a937eb3777876ac539b905adc0944
hk416Glitch <azovdemon762@gmail.com>
2025-08-10 16:35:36 -0600

    Update vite.config.js6

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
   }
-};
+});
