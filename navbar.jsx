f28294e297142ee4ec0760529117b3db8cd85cf7
hk416Glitch <azovdemon762@gmail.com>
2025-08-10 10:55:08 -0600

    Update navbar.jsx1

--- navbar.jsx
+++ navbar.jsx
@@ -1,16 +1,7 @@
-import React from "react";
-
 export default function Navbar() {
   return (
-    <nav className="bg-green-600 text-white p-4 shadow-lg">
-      <div className="container mx-auto flex justify-between">
-        <h1 className="text-2xl font-bold">Exotic Snack Shop</h1>
-        <ul className="flex space-x-6">
-          <li><a href="#" className="hover:underline">Home</a></li>
-          <li><a href="#" className="hover:underline">Shop</a></li>
-          <li><a href="#" className="hover:underline">Contact</a></li>
-        </ul>
-      </div>
+    <nav className="bg-purple-600 text-white p-4">
+      <h1 className="text-xl font-bold">Exotic Snack Shop</h1>
     </nav>
   );
 }
