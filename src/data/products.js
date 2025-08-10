ee0c501741086f7c78bb3cf84b8e04a30b52328d
hk416Glitch <azovdemon762@gmail.com>
2025-08-10 12:40:34 -0600

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 1,
+    name: "Exotic Snack Gift Pack",
+    category: "Exotic Snacks",
+    price: 24.99,
+    image: "/images/exotic-snack-pack.jpg",
+  },
+  {
+    id: 2,
+    name: "Alcohol Gift Pack",
+    category: "Alcohol",
+    price: 49.99,
+    image: "/images/alcohol-pack.jpg",
+  },
+  {
+    id: 3,
+    name: "Alcohol + Exotic Snack Pack",
+    category: "Mix",
+    price: 59.99,
+    image: "/images/mix-pack.jpg",
+  },
+  {
+    id: 4,
+    name: "Imported Chips",
+    category: "Exotic Snacks",
+    price: 4.99,
+    image: "/images/imported-chips.jpg",
+  },
+];
+
+export default products;
